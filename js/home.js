e665f83f305e2adc72621e0a1df9f526e9a56039
Skedaddlez <46994598+Skedaddlez@users.noreply.github.com>
2019-02-21 14:14:10 +1100

    update

--- js/home.js
+++ js/home.js
@@ -2,11 +2,14 @@
 // When the user scrolls the page, execute myFunction 
 window.onscroll = function () { myFunction() };
 
+
 // Get the navbar
 var navbar = document.getElementById("navbar");
 
 // Get the offset position of the navbar
 var sticky = navbar.offsetTop;
+
+
 
 // Add the sticky class to the navbar when you reach its scroll position. Remove "sticky" when you leave the scroll position
 function myFunction() {
@@ -16,3 +19,4 @@ function myFunction() {
         navbar.classList.remove("sticky");
     }
 }
+
