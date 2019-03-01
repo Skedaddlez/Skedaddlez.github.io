1f25315e8f518bfbecfe6429e68a41e491efd775
Skedaddlez <Skedaddlez@users.noreply.github.com>
2019-03-02 00:51:14 +1100

    update

--- js/other.js
+++ js/other.js
@@ -3,9 +3,9 @@
 function projclick(x,y) {
     var activep = document.getElementById("activeproject")
     var activept = document.getElementById("activeprojecttext")
-    var activelk = document.getElementById("activeprojectlink")
+    var activelk = document.getElementById("activeprojectlink")    
     
-    activep.style.width = "92%";
+    activep.style.height = "400px";
     activep.style.padding = "2%";    
     activept.innerHTML = document.getElementById(x).innerHTML;
     activep.style.backgroundImage = document.getElementById(y).style.backgroundImage;  
