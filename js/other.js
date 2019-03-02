c972d4107877738a82574a5b8819e88f76ffcb08
Skedaddlez <Skedaddlez@users.noreply.github.com>
2019-03-02 22:04:54 +1100

    update

--- js/other.js
+++ js/other.js
@@ -1,12 +1,17 @@
 // JavaScript source code
 
-function projclick(x,y) {
+function projclick(x,y,z) {
     var activep = document.getElementById("activeproject")
     var activept = document.getElementById("activeprojecttext")
-    var activelk = document.getElementById("activeprojectlink")    
-    
+    var activelk = document.getElementById("activeprojectlink")
+
+    var activelk2 = document.getElementById(z)
+    var link = activelk2.getAttribute("href");    
+
     activep.style.maxHeight = "1000px";
-    activep.style.padding = "2%";    
+    activep.style.padding = "2%";
+    activep.setAttribute("onclick", "window.open('" + link +"')");
+    activep.setAttribute("target", "_blank");
     activept.innerHTML = document.getElementById(x).innerHTML;
     activep.style.backgroundImage = document.getElementById(y).style.backgroundImage;  
       
